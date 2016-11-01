6831f44e6df289072d7736dde32d9404856201ba
Yves Schleich <yves.schleich@gmail.com>
2016-11-01 23:31:42 +0100

    Changed ui components from component to const

--- src/ui/SecretSantaCreator/SecretSantaCreator.js
+++ src/ui/SecretSantaCreator/SecretSantaCreator.js
@@ -1,23 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Row, Col} from 'react-bootstrap';
 
 import Input from 'react-toolbox/lib/input';
 import Button from 'react-toolbox/lib/button';
 
-export class SecretSantaCreator extends Component {
-    render() {
-        return (
-            <Row className="show-grid">
-                <Col xs={12} sm={5}>
-                    <Input type='text' label='Name' name='name'/>
-                </Col>
-                <Col xs={12} sm={5}>
-                    <Input type='email' label='E-Mail' name='E-Mail'/>
-                </Col>
-                <Col xs={12} sm={2}>
-                    <Button icon='add' floating />
-                </Col>
-            </Row>
-        );
-    }
+export const SecretSantaCreator = (props) => {
+    return (
+        <Row className="show-grid">
+            <Col xs={12} sm={5}>
+                <Input type='text' label='Name' name='name'/>
+            </Col>
+            <Col xs={12} sm={5}>
+                <Input type='email' label='E-Mail' name='E-Mail'/>
+            </Col>
+            <Col xs={12} sm={2}>
+                <Button icon='add' floating />
+            </Col>
+        </Row>
+    );
 };
--- src/ui/Sender/Sender.js
+++ src/ui/Sender/Sender.js
@@ -1,29 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import Input from 'react-toolbox/lib/input';
 import {Row, Col} from 'react-bootstrap';
 
-export class Sender extends Component {
-    render() {
-        return(
-            <div>
-                <Row>
-                    <Col xs={12}>
-                        <h2><smal>Absender</smal></h2>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} sm={4}>
-                        <Input type='email' label='E-Mail' name='email'/>
-                    </Col>
-                    <Col xs={12} sm={4}>
-                        <Input type='text' label='Name' name='name'/>
-                    </Col>
-                    <Col xs={12} sm={4}>
-                        <Input type='text' label='Nachricht' name='message'/>
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
-}
+export const Sender = (props) => {
+    return(
+        <div>
+            <Row>
+                <Col xs={12}>
+                    <h2><smal>Absender</smal></h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={4}>
+                    <Input type='email' label='E-Mail' name='email'/>
+                </Col>
+                <Col xs={12} sm={4}>
+                    <Input type='text' label='Name' name='name'/>
+                </Col>
+                <Col xs={12} sm={4}>
+                    <Input type='text' label='Nachricht' name='message'/>
+                </Col>
+            </Row>
+        </div>
+    );
+};
